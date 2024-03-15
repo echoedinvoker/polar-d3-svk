01194434a9360e0ea13440ed853234c5a905fb2a
Matt Chang <echoedinvoker@gmail.com>
2024-03-15 13:32:05 +0800

    update

--- src/lib/components/Controller.svelte
+++ src/lib/components/Controller.svelte
@@ -3,7 +3,7 @@
   import { inputs } from "../../routes/inputs";
 </script>
 
-<div class="row center">
+<div class="row center center-on-small">
   {#if $inputs.name && $inputs.value}
     <div class="col s4">
       {#if $data.map((d) => d.name).includes($inputs.name)}
@@ -57,3 +57,29 @@
     >
   </div>
 </div>
+
+<style>
+  @media (max-width: 1500px) {
+    .row.center-on-small {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      width: 100%; /* 確保在小螢幕上佔滿父元素寬度 */
+    }
+
+    .row.row.center-on-small .col {
+      width: 100%; /* 確保在小螢幕上佔滿父元素寬度 */
+      margin-bottom: 10px; /* 在按鈕之間添加間隔 */
+      margin-left: 18px;
+    }
+
+    .row.center-on-small .btn {
+      width: 100%; /* 使按鈕寬度匹配父元素寬度 */
+    }
+
+    .row.center-on-small .col:last-child {
+      margin-bottom: 0; /* 移除最後一個按鈕的間隔，保持美觀 */
+    }
+  }
+</style>
